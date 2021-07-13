3388d55bd1d0f2842e6f9e145225150ac79bafa9
Dinara <ditiviola@gmail.com>
2021-07-13 13:25:45 +0300

    sd

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-          First Remote Commit
+          The Third Local Commit
         </a>
       </header>
     </div>
