07b9ae8c9ed7fea6278f494576f2e405ffb3bc87
Dinara <ditiviola@gmail.com>
2021-07-16 11:26:56 +0300

    new message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          The TSecond Branch
+          The Second Branch
         </a>
       </header>
     </div>
